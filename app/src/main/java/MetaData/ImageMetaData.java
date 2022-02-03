8ecaa48ccf3b72c2f1c3cfc76f8993b2722e5f1b
= <rizal.solehudin45@gmail.com>
2022-02-03 11:47:38 +0700

    Meta data extractor

---
+++ app/src/main/java/MetaData/ImageMetaData.java
@@ -0,0 +1,40 @@
+package MetaData;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static com.drew.metadata.exif.ExifDirectoryBase.TAG_DATETIME_ORIGINAL;
+
+public class ImageMetaData {
+    Metadata metadata;
+    Date date;
+
+    public ImageMetaData(File file) throws Exception {
+        metadata = ImageMetadataReader.readMetadata(file);
+        ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+
+        date = directory.getDate(TAG_DATETIME_ORIGINAL);
+    }
+
+    public String getImageTime() {
+        return dateFormat("HH:mm:ss");
+    }
+    public String getImageDate()
+    {
+        return dateFormat("yyyy-mm-dd");
+    }
+
+
+    public String dateFormat(String format) {
+        SimpleDateFormat formatter = new SimpleDateFormat(format, Locale.US);
+        return formatter.format(date);
+    }
+
+
+}
--- app/src/main/java/com/tamamura/qisen/MainActivity.java
+++ app/src/main/java/com/tamamura/qisen/MainActivity.java
@@ -1,14 +1,47 @@
 package com.tamamura.qisen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int REQUEST_CODE_PERMISSIONS = 1001;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE","android.permission.INTERNET"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private boolean allPermissionsGranted(){
+
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                logic();
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
+    }
+
+    public void logic()
+    {
+
+    }
+
 }
