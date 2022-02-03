e6f5c9302ff8a9436ef013dc7a62db251d8c751e
= <rizal.solehudin45@gmail.com>
2022-02-03 14:01:28 +0700

    Some asset

---
+++ app/src/main/java/Client/UserClient.java
@@ -0,0 +1,118 @@
+package Client;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+
+public class UserClient {
+    public String userImg = "img/user/";
+    public String userDefaultImage = "img/user/default.jpg";
+
+    public UserClient()
+    {
+
+    }
+
+    public String get(String destination) throws  Exception
+    {
+        String res = "";
+        URL url = new URL(destination);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        con.setDoInput(true);
+        con.setDoOutput(true);
+        con.setConnectTimeout(270000);
+        con.setReadTimeout(270000);
+
+
+        int statusCode = con.getResponseCode();
+
+        if( statusCode == HTTP_OK)
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                res += line;
+            }
+            br.close();
+        }else
+        {
+            res = "";
+        }
+
+        return res;
+    }
+
+    public String post( String destination, HashMap<String, String> param) throws Exception
+    {
+        String res = "";
+
+        URL url = new URL(destination);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoInput(true);
+        con.setDoOutput(true);
+
+        con.setReadTimeout(270000);
+        con.setConnectTimeout(270000);
+
+        OutputStream os = con.getOutputStream();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
+
+        bw.write(getParam(param));
+        bw.flush();
+        bw.close();
+
+        os.close();
+
+        int statusCode = con.getResponseCode();
+        if( statusCode == HTTP_OK)
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                res += line;
+            }
+        }else
+        {
+            res = "";
+        }
+
+        return res;
+    }
+
+    public String getParam(HashMap<String, String> param)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        boolean status = true;
+        try{
+            for(Map.Entry<String, String> map : param.entrySet())
+            {
+                if( status )
+                    status = false;
+                else
+                    sb.append("&");
+                sb.append(URLEncoder.encode(map.getKey(), "UTF-8"));
+                sb.append("=");
+                sb.append(URLEncoder.encode(map.getValue(), "UTF-8"));
+            }
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+}
