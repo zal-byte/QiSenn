a79ef67f21572bcf600610485c246c2046a9716e
= <rizal.solehudin45@gmail.com>
2022-02-03 15:37:47 +0700

    SIODFJSOIDJFSDOIF

--- app/src/main/java/com/tamamura/qisen/MainActivity.java
+++ app/src/main/java/com/tamamura/qisen/MainActivity.java
@@ -8,17 +8,40 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int REQUEST_CODE_PERMISSIONS = 1001;
     private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE","android.permission.INTERNET"};
+    LinearLayout layout1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermission(REQUEST_CODE_PERMISSIONS);
+
+        getWindow().setLayout(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        init();
+        logic();
+
+        Animation slide_up = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_up);
+
+        new Handler().postDelayed(() -> layout1.setAnimation(slide_up),2000);
+
     }
+    private void init()
+    {
+        layout1 = findViewById(R.id.layout1);
+
+    }
+
     private boolean allPermissionsGranted(){
 
         for(String permission : REQUIRED_PERMISSIONS){
