8ef4fed2c29db2e9ca7d5bb4a8c11d766a47ebe6
= <rizal.solehudin45@gmail.com>
2022-02-24 12:10:49 +0700

    JSODIJF

---
+++ app/src/main/java/com/tamamura/qisen/admin/TampilUser.java
@@ -0,0 +1,16 @@
+package com.tamamura.qisen.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tamamura.qisen.R;
+
+public class TampilUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil_user);
+    }
+}
---
+++ app/src/main/java/com/tamamura/qisen/admin/UbahUser.java
@@ -0,0 +1,17 @@
+package com.tamamura.qisen.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tamamura.qisen.R;
+
+public class UbahUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_user);
+    }
+
+}
